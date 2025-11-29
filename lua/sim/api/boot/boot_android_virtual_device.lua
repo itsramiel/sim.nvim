a1259cabde44b15712b79a4178198cec03c09053
Rami Elwan <ramielwan48@gmail.com>
2025-11-29 17:01:42 +0100

    feat: update list without refresh

--- lua/sim/api/boot/boot_android_virtual_device.lua
+++ lua/sim/api/boot/boot_android_virtual_device.lua
@@ -1,27 +1,34 @@
+local coop = require("coop")
+local cvim = require("coop.vim")
 local coop_utils = require("coop.uv-utils")
 local executables = require("sim.api.executables")
 local get_booted_android_emulator_names = require("sim.api.get_booted_android_emulator_names")
 
 ---async
 ---@param avd_name string
----@return  boolean
+---@return string?
 local function boot_android_virtual_device(avd_name)
-	if executables.emulator == nil then
-		return false
-	end
+  if executables.emulator == nil then
+    return nil
+  end
 
-	vim.system({ executables.emulator, "-avd", avd_name }, { detach = true })
+  coop.spawn(function()
+    cvim.system({ executables.emulator, "-avd", avd_name }, { detach = true })
+  end)
 
-	for i = 1, 30 do
-		local booted_android_emulator_names = get_booted_android_emulator_names()
-		if booted_android_emulator_names[avd_name] then
-			return true
-		else
-			coop_utils.sleep(100)
-		end
-	end
+  for i = 1, 30 do
+    local booted_android_emulator_names = get_booted_android_emulator_names()
+    local adb_name = booted_android_emulator_names[avd_name]
+    if adb_name ~= nil then
+      print("gottem")
+      return adb_name
+    else
+      coop_utils.sleep(100)
+    end
+  end
+  print("yo")
 
-	return false
+  return nil
 end
 
 return boot_android_virtual_device
--- lua/sim/integrations/snacks/init.lua
+++ lua/sim/integrations/snacks/init.lua
@@ -4,141 +4,157 @@ local coop = require("coop")
 local Snacks = require("snacks")
 local api = require("sim.api")
 local models = require("sim.api.models")
-local boot = require("sim.api.boot")
+local boot_ios_virtual_device = require("sim.api.boot.boot_ios_virtual_device")
+local boot_android_virtual_device = require("sim.api.boot.boot_android_virtual_device")
 local shutdown = require("sim.api.shutdown")
 
 local function get_icon_for_device(device)
-	if models.IOSVirtualDevice.is_ios_virtual_device(device) then
-		return ""
-	elseif models.AndroidVirtualDevice.is_android_virtual_device(device) then
-		return ""
-	else
-		return nil
-	end
+  if models.IOSVirtualDevice.is_ios_virtual_device(device) then
+    return ""
+  elseif models.AndroidVirtualDevice.is_android_virtual_device(device) then
+    return ""
+  else
+    return nil
+  end
 end
 
 local function show_devices_picker()
-	Snacks.picker({
-		show_ios = true,
-		show_android = true,
-		finder = function(opts, ctx)
-			return function(callback)
-				coop.spawn(function()
-					local ios_task = coop.spawn(function()
-						local show_ios = opts.show_ios
-						if show_ios == false then
-							return
-						end
-						local devices = api.get_ios_virtual_devices()
+  Snacks.picker({
+    show_ios = true,
+    show_android = true,
+    finder = function(opts, ctx)
+      return function(callback)
+        coop.spawn(function()
+          local ios_task = coop.spawn(function()
+            local show_ios = opts.show_ios
+            if show_ios == false then
+              return
+            end
+            local devices = api.get_ios_virtual_devices()
 
-						for _, device in ipairs(devices) do
-							---@type snacks.picker.finder.Item
-							local item = {
-								text = device.name,
-								device = device,
-							}
-							callback(item)
-						end
-					end)
+            for _, device in ipairs(devices) do
+              ---@type snacks.picker.finder.Item
+              local item = {
+                text = device.name,
+                device = device,
+              }
+              callback(item)
+            end
+          end)
 
-					local android_task = coop.spawn(function()
-						local show_android = opts.show_android
-						if show_android == false then
-							return
-						end
-						local devices = api.get_android_virtual_devices()
-						for _, device in ipairs(devices) do
-							---@type snacks.picker.finder.Item
-							local item = {
-								text = device.name,
-								device = device,
-							}
-							callback(item)
-						end
-					end)
+          local android_task = coop.spawn(function()
+            local show_android = opts.show_android
+            if show_android == false then
+              return
+            end
+            local devices = api.get_android_virtual_devices()
+            for _, device in ipairs(devices) do
+              ---@type snacks.picker.finder.Item
+              local item = {
+                text = device.name,
+                device = device,
+              }
+              callback(item)
+            end
+          end)
 
-					ctx.async:on("abort", function()
-						if ios_task:is_cancelled() == false then
-							ios_task:cancel()
-						end
-						if android_task:is_cancelled() == false then
-							android_task:cancel()
-						end
-					end)
+          ctx.async:on("abort", function()
+            if ios_task:is_cancelled() == false then
+              ios_task:cancel()
+            end
+            if android_task:is_cancelled() == false then
+              android_task:cancel()
+            end
+          end)
 
-					ios_task:await()
-					android_task:await()
-					ctx.async:resume()
-				end)
+          ios_task:await()
+          android_task:await()
+          ctx.async:resume()
+        end)
 
-				ctx.async:suspend()
-			end
-		end,
-		format = function(item, _)
-			local name = item.text
-			local icon = get_icon_for_device(item.device)
-			local booted = false
+        ctx.async:suspend()
+      end
+    end,
+    format = function(item, _)
+      local name = item.text
+      local icon = get_icon_for_device(item.device)
+      local booted = false
 
-			local device = item.device
+      local device = item.device
 
-			if models.IOSVirtualDevice.is_ios_virtual_device(device) then
-				---@cast device IOSVirtualDevice
-				booted = device.state == "Booted"
-			elseif models.AndroidVirtualDevice.is_android_virtual_device(device) then
-				---@cast device AndroidVirtualDevice
-				booted = device.adb_name ~= nil
-			end
+      if models.IOSVirtualDevice.is_ios_virtual_device(device) then
+        ---@cast device IOSVirtualDevice
+        booted = device.state == "Booted"
+      elseif models.AndroidVirtualDevice.is_android_virtual_device(device) then
+        ---@cast device AndroidVirtualDevice
+        booted = device.adb_name ~= nil
+      end
 
-			local text = icon .. "  " .. name
-			if booted then
-				text = text .. " ✅"
-			end
+      local text = icon .. "  " .. name
+      if booted then
+        text = text .. " ✅"
+      end
 
-			return {
-				{ text },
-			}
-		end,
-		toggles = {
-			show_ios = "I",
-			show_android = "A",
-		},
-		actions = {
-			boot = function(self, item)
-				coop.spawn(function()
-					local device = item.device
-					local success = boot.boot_device(device)
-					if success then
-						self:refresh()
-					end
-				end)
-			end,
-			shutdown = function(self, item)
-				coop.spawn(function()
-					local device = item.device
-					local success = shutdown.shutdown_device(device)
+      return {
+        { text },
+      }
+    end,
+    toggles = {
+      show_ios = "I",
+      show_android = "A",
+    },
+    actions = {
+      boot = function(self, item)
+        coop.spawn(function()
+          local device = item.device
+          if models.IOSVirtualDevice.is_ios_virtual_device(device) then
+            ---@cast device IOSVirtualDevice
+            local success = boot_ios_virtual_device(device.udid)
+            print(success)
+            if success then
+              device.state = "Booted"
+              self.list:update({ force = true })
+            end
+          elseif models.AndroidVirtualDevice.is_android_virtual_device(device) then
+            print("android")
+            ---@cast device AndroidVirtualDevice
+            local adb_name = boot_android_virtual_device(device.name)
+            print("rami")
+            if adb_name ~= nil then
+              device.adb_name = adb_name
+              print("update")
+              self.list:update({ force = true })
+            end
+          end
+        end)
+      end,
+      shutdown = function(self, item)
+        coop.spawn(function()
+          local device = item.device
+          local success = shutdown.shutdown_device(device)
 
-					if success then
-						self:refresh()
-					end
-				end)
-			end,
-		},
-		win = {
-			input = {
-				keys = {
-					["<C-b>"] = { "boot", mode = { "n", "i" } },
-					["<C-x>"] = { "shutdown", mode = { "n", "i" } },
-					["<C-i>"] = { "toggle_show_ios", mode = { "n", "i" } },
-					["<C-a>"] = { "toggle_show_android", mode = { "n", "i" } },
-				},
-			},
-		},
-		layout = { preset = "select" },
-	})
+          if success then
+            self:refresh()
+          end
+        end)
+      end,
+    },
+    win = {
+      input = {
+        keys = {
+          ["<C-b>"] = { "boot", mode = { "n", "i" } },
+          ["<C-x>"] = { "shutdown", mode = { "n", "i" } },
+          ["<C-i>"] = { "toggle_show_ios", mode = { "n", "i" } },
+          ["<C-a>"] = { "toggle_show_android", mode = { "n", "i" } },
+        },
+      },
+    },
+    layout = { preset = "select" },
+  })
 end
 
 M.setup = function()
-	M.show_devices_picker = show_devices_picker
+  M.show_devices_picker = show_devices_picker
 end
 
 return M
